923b77aab6f271e34fb1a214f3dd34725f174844
Pemenang <salampemenang2017@gmail.com>
2019-04-12 02:06:32 +0700

    Login And register front end

---
+++ resources/views/partials/assets-head.blade.php
@@ -0,0 +1,38 @@
+    <!-- Bootstrap CSS
+        ============================================ -->
+    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+    <!-- font awesome CSS
+        ============================================ -->
+    <link rel="stylesheet" href="{{asset('assets/css/font-awesome.min.css')}}">
+    <!-- owl.carousel CSS
+        ============================================ -->
+    <link rel="stylesheet" href="{{asset('assets/css/owl.carousel.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/owl.theme.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/owl.transitions.css')}}">
+    <!-- animate CSS
+        ============================================ -->
+    <link rel="stylesheet" href="{{asset('assets/css/animate.css')}}">
+    <!-- normalize CSS
+        ============================================ -->
+    <link rel="stylesheet" href="{{asset('assets/css/normalize.css')}}">
+    <!-- mCustomScrollbar CSS
+        ============================================ -->
+    <link rel="stylesheet" href="{{asset('assets/css/scrollbar/jquery.mCustomScrollbar.min.css')}}">
+    <!-- wave CSS
+        ============================================ -->
+    <link rel="stylesheet" href="{{asset('assets/css/wave/waves.min.css')}}">
+    <!-- Notika icon CSS
+        ============================================ -->
+    <link rel="stylesheet" href="{{asset('assets/css/notika-custom-icon.css')}}">
+    <!-- main CSS
+        ============================================ -->
+    <link rel="stylesheet" href="{{asset('assets/css/main.css')}}">
+    <!-- style CSS
+        ============================================ -->
+    <link rel="stylesheet" href="{{asset('assets/style.css')}}">
+    <!-- responsive CSS
+        ============================================ -->
+    <link rel="stylesheet" href="{{asset('assets/css/responsive.css')}}">
+    <!-- modernizr JS
+        ============================================ -->
+    <script src="{{asset('assets/js/vendor/modernizr-2.8.3.min.js')}}"></script>
