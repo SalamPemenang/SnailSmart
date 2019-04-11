446a103620fc93602c9627cfde7fe4142c930d53
Dani Ramdani <daniramdani121201@gmail.com>
2019-04-11 23:31:57 +0700

    Multi auth

--- app/Http/Controllers/Admin/AdminLoginController.php
+++ app/Http/Controllers/Admin/AdminLoginController.php
@@ -4,8 +4,35 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Auth;
 
 class AdminLoginController extends Controller
 {
-    //
+    public function __construct()
+    {
+    	$this->middleware('guest:admin');
+    }
+
+    public function index()
+    {
+    	return view('admin.login');
+    }
+
+    public function login(Request $request)
+    {
+    	// Validate
+    	$this->validate($request,[
+    		'email' 	=> 'required|email',
+    		'password'  => 'required|min:6'
+    	]);
+
+    	// Attempt
+    	if(Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password], $request->remember)){
+    		// Jika Berhasil
+    		return redirect()->route('admin-dashboard');
+    	}
+    	// Jika Gagal
+    	return redirect()->back()->withInput($request->only('email', 'remember'));
+    	
+    }
 }
