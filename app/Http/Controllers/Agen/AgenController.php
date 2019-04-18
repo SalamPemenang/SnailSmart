2c8c46ede1dbe3ff571aca27cd62589de8ccaac4
Dani Ramdani <daniramdani121201@gmail.com>
2019-04-19 03:05:46 +0700

    Agen Backend

--- app/Http/Controllers/Agen/AgenController.php
+++ app/Http/Controllers/Agen/AgenController.php
@@ -23,32 +23,93 @@ class AgenController extends Controller
 
     public function searchUser()
     {
-    	$user = User::all();
+    	$user = User::paginate(10);
     	return view('agen.search', ['user' => $user]);
     }
 
-    public function getUser($id)
+    public function getUser(Request $request)
     {
-    	$user = User::find($id);
-    	return view('agen.transfer-user.transfer', ['user' => $user]);
+    	$user = User::where('name', 'like', '%'.request('name').'%')->where('virtual_account', 'like', '%'.request('virtual_account').'%')->paginate(10);
+        return view('agen.search', ['user' => $user]);	
+    }
+
+    public function formSaldo($id)
+    {
+        $agen = Agen::find($id);
+        $user = User::find($id);
+        return view('agen.transfer-user.transfer', ['user' => $user, 'agen' => $agen]);
+    }
+
+    public function formNabung($id)
+    {
+        $agen = Agen::find($id);
+        $user = User::find($id);
+        return view('agen.nabung.transfer', ['user' => $user, 'agen' => $agen]);
     }
     
     public function transferSaldo(Request $request)
     {
     	
     	$id = $request->get('id');
-			if($id){
-				$user = User::find($id);	
-			}
-    	
-    	$transaction = new Transaction;
+        $a = $request->get('agen_id');
+        $b = $request->get('totalagen');
+        $c = $request->get('saldo');
+        $d = $request->get('saldouser');
 
+		if($id){
+			$user = User::find($id);	
+		}
+            $agen = Agen::find($id);
+            $transaction = new Transaction;        
+    	
+
+        $transaction->user_id = $request->user_id;
     	$transaction->debit = $request->saldo;
     	$transaction->save();
 
-    	$user->saldo = $request->saldo;
+        $totala = $b - $c;
+
+        $totalb = $d + $c;
+
+    	$user->saldo = $totalb;
     	$user->save();
 
+        $agen->saldo = $totala;
+        $agen->save();
+
     	return redirect()->route('agen.dashboard');
+    }
+
+    public function transferNabung(Request $request)
+    {
+
+        $id = $request->get('id');
+        $a = $request->get('agen_id');
+        $b = $request->get('totalagen');
+        $c = $request->get('saldo');
+        $d = $request->get('saldouser');
+
+        if($id){
+            $user = User::find($id);    
+        }
+            $agen = Agen::find($id);
+            $transaction = new Transaction;        
+        
+
+        $transaction->user_id = $request->user_id;
+        $transaction->debit = $request->saldo;
+        $transaction->save();
+
+        $totala = $b - $c;
+
+        $totalb = $d + $c;
+
+        $user->saldo = $totalb;
+        $user->save();
+
+        $agen->saldo = $totala;
+        $agen->save();
+
+        return redirect()->route('agen.dashboard');
     }
 }
